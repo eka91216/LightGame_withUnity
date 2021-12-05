8c30aed8ba546d8dbe1660c8d0af860b389b58cb
YEDAM <79961930+eka91216@users.noreply.github.com>
2021-12-06 06:56:35 +0900

    Make MainPanel and SettingPanel

---
+++ Assets/GameSetPanel.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSetPanel : MonoBehaviour
+{
+    public Text m_BlankText;
+    public GameObject m_CurrentPanel;
+    public GameObject m_NextPanel;
+    public GameObject m_BeforePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.transform.gameObject.tag == "MapChoice")
+                {
+                    m_BlankText.text = raycastHit.transform.gameObject.name;
+                }
+            }
+        }
+    }
+
+    public void SetButton()
+    {
+        if (m_BlankText.text != "")
+        {
+            m_BlankText.text = "";
+            m_CurrentPanel.SetActive(false);
+            m_NextPanel.SetActive(true);
+        }
+    }
+
+    public void BackButton()
+    {
+        m_BlankText.text = "";
+        m_CurrentPanel.SetActive(false);
+        m_BeforePanel.SetActive(true);
+    }
+}
